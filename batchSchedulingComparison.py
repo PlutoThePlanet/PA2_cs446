9d3d7453a5403ed5538cf603168271a01d671f8f
PlutoThePlanet <pmortensen0916@gmail.com>
2022-03-19 12:00:34 -0700

    main done

--- batchSchedulingComparison.py
+++ batchSchedulingComparison.py
@@ -6,58 +6,87 @@
 # Be sure to explain where each would be appropriate to use,
 # and any possible tradeoffs in implementation or process execution
 
+import sys
+from DistUpgrade.DistUpgradeViewText import readline
 
-def main():
-    # from terminal: enter the program name, batchfileName, and type of process sort you would like to do
-    #   for example: python3 batchSchedulingComparison.py batchfile.txt
 
-    # If the user does not enter your three arguments (program name batchfileName and sortType),
-    #   then you should prompt the user repeatedly and take their input until they enter the correct number of arguments
-    # Once the user supplies the correct number of arguments
-    #   use argv to get the batch file name, and then read all the data from it, if you can
-    # If you can't (because the user entered a non-existing file name)
-    #   tell the user that the file doesn't exist and exit the program
-    # If you're able to read from the file name provided by the user
-    #   then you should get the algorithm name from the argv list
-    # If the user does not provide one of these arguments
-    #   tell the user that their process scheduling options are FCFS, ShortestFirst, or Priority, and exit the program
-    # If the user enters an acceptable algorithm name
-    #   perform a logical check to see which function you should call
+def avg_turnaround():
+    # Turnaround time is calculated by subtracting each processCompletionTime from its arrivalTime
+    #   For example, using FCFS (see below) process 3 takes 50 seconds to execute and arrived at time 0,
+    #   so process 3 has a turnaround time of 70 because it has to wait 20 seconds for process 1 to fully execute
+    # To calculate the average, sum each process' turnaround time, and divide by the number of processes
+    #   So if we only executed process 1 and 3, we would add 20 and 70 and divide by 2- the turnaround time of those
+    #   two processes averaged (ignoring the rest of the list for simplicity) is 45
+    return
 
+
+def avg_wait():
+    # Wait time is calculated by subtracting each processBurstTime from its processTurnaroundTime
+    #   For example, using FCFS (see below) we previously calculated that process 3 has a turnaround time of 70, and
+    #   process 1 has a turnaround time of 20. To calculate the waitTime for process 3, we subtract the burst time
+    #   from the turnaround (70-50) and get 20; doing the same for process 1, we get 0
+    # To calculate the average, sum each process' wait time, and divide by the number of processes
+    #   So if we only executed process 1 and 3, we would add 20 and 0 and divide by 2
+    #   the wait time of those two processes averaged (ignoring the rest of the list for simplicity) is 10
+    return
+
+
+def first_come_first_served_sort():
+    #
+    return
+
+
+def shortest_job_first_sort():
+    #
+    return
+
+
+def priority_sort():
+    #
+    return
+
+
+def main(argv):
+    sorting = {"FCFS", "ShortestFirst", "Priority"}
+
+    if len(sys.argv) != 3:
+        print("you did not enter the correct number of arguments")
+        print("what is the name of your process file?")
+        file = input()
+        sys.argv[1] = file
+        print("what sort type would you like to use?")
+        sort = input()
+        sys.argv[2] = sort
+
+    if not open(sys.argv[1], 'r'):                                                      # does this file exist
+        print("your file doesn't exist")
+        quit()
+    else:                                                                               # then parse the input
+        with open(sys.argv[1], 'r') as f:
+            for line in f:
+                print(line)
+                pid, arrival_time, burst_time, priority = line.split()
+
+    if not any(ele in sys.argv[2] for ele in sorting):                                  # did you provide a sort
+        print("your process scheduling options are FCFS, ShortestFirst, or Priority")
+        quit()
+
+    if sys.argv[2] == "FCFS":                                                           # call sorting and print
+        first_come_first_served_sort()
+    elif sys.argv[2] == "ShortestFirst":
+        shortest_job_first_sort()
+    elif sys.argv[2] == "Priority":
+        priority_sort()
     # For each algorithm, the output to the terminal should be the processes in the order that they should execute,
     # the average process waiting time, and the average process turn around time, each on their own line. All input
     # and output should be gathered and executed IN MAIN
-
     # Please print to 2 decimal places
 
-    def avg_turnaround():
-        # Turnaround time is calculated by subtracting each processCompletionTime from its arrivalTime
-        #   For example, using FCFS (see below) process 3 takes 50 seconds to execute and arrived at time 0,
-        #   so process 3 has a turnaround time of 70 because it has to wait 20 seconds for process 1 to fully execute
-        # To calculate the average, sum each process' turnaround time, and divide by the number of processes
-        #   So if we only executed process 1 and 3, we would add 20 and 70 and divide by 2- the turnaround time of those
-        #   two processes averaged (ignoring the rest of the list for simplicity) is 45
-        return
 
-    def avg_wait():
-        # Wait time is calculated by subtracting each processBurstTime from its processTurnaroundTime
-        #   For example, using FCFS (see below) we previously calculated that process 3 has a turnaround time of 70, and
-        #   process 1 has a turnaround time of 20. To calculate the waitTime for process 3, we subtract the burst time
-        #   from the turnaround (70-50) and get 20; doing the same for process 1, we get 0
-        # To calculate the average, sum each process' wait time, and divide by the number of processes
-        #   So if we only executed process 1 and 3, we would add 20 and 0 and divide by 2
-        #   the wait time of those two processes averaged (ignoring the rest of the list for simplicity) is 10
-        return
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
-    def first_come_first_served_sort():
-        #
-        return
-
-    def shortest_job_first_sort():
-        #
-        return
-
-    def priority_sort():
-        #
-        return
-    
+# print("start test")
+# print(len(sys.argv))
+# print(str(sys.argv))
+# print("end test")
