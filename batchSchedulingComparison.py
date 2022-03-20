ac607f045aa7fdd21fa05a4465a2fd3a16ec6f60
PlutoThePlanet <pmortensen0916@gmail.com>
2022-03-19 17:04:22 -0700

    fixed list of strings to list of ints. can now pass completion time list

--- batchSchedulingComparison.py
+++ batchSchedulingComparison.py
@@ -9,17 +9,24 @@
 import sys
 
 
-def avg_turnaround():
-    # Turnaround time is calculated by subtracting each processCompletionTime from its arrivalTime
-    #   For example, using FCFS (see below) process 3 takes 50 seconds to execute and arrived at time 0,
-    #   so process 3 has a turnaround time of 70 because it has to wait 20 seconds for process 1 to fully execute
-    # To calculate the average, sum each process' turnaround time, and divide by the number of processes
-    #   So if we only executed process 1 and 3, we would add 20 and 70 and divide by 2- the turnaround time of those
-    #   two processes averaged (ignoring the rest of the list for simplicity) is 45
-    return
+def avg_turnaround(process_completion_times, process_arrival_times):
+    turnaround_times = []
+    time_total = 0
+    for elem in range(0, len(process_completion_times)):
+        time = process_completion_times[elem] - process_arrival_times[elem]         # take the difference
+        turnaround_times += [time]                                                  # add val to list
+        time_total += time                                                          # add to total
+    avg_turn_time = time_total / len(turnaround_times)                              # calc avg
+    print([turnaround_times, avg_turn_time])
+    return [turnaround_times, avg_turn_time]
 
 
-def avg_wait():
+def avg_wait(process_turnaround_times, process_burst_times):
+    wait_total = 0
+    for elem in range(0, len(process_turnaround_times)):
+        wait_time = process_turnaround_times[elem] - process_burst_times[elem]
+        wait_total += wait_time
+        # wait_avg = wait_total /
     # Wait time is calculated by subtracting each processBurstTime from its processTurnaroundTime
     #   For example, using FCFS (see below) we previously calculated that process 3 has a turnaround time of 70, and
     #   process 1 has a turnaround time of 20. To calculate the waitTime for process 3, we subtract the burst time
@@ -27,22 +34,22 @@ def avg_wait():
     # To calculate the average, sum each process' wait time, and divide by the number of processes
     #   So if we only executed process 1 and 3, we would add 20 and 0 and divide by 2
     #   the wait time of those two processes averaged (ignoring the rest of the list for simplicity) is 10
-    return
+    return []
 
 
 def first_come_first_served_sort():
     #
-    return
+    return []
 
 
 def shortest_job_first_sort():
     #
-    return
+    return []
 
 
 def priority_sort():
     #
-    return
+    return []
 
 
 def main():
@@ -51,6 +58,7 @@ def main():
     arrival_time_list = []
     burst_time_list = []
     priority_list = []
+    completion_list = []
 
     if len(sys.argv) != 3:
         print("you did not enter the correct number of arguments")
@@ -59,7 +67,7 @@ def main():
         sys.argv[1] = file
         print("what sort type would you like to use?")
         sort = input()
-        sys.argv[2] = sort                                                              # INDEX 2 DOESN'T EXIST HERE ???
+        sys.argv[2] = sort                                                       # ERROR: INDEX 2 DOESN'T EXIST HERE ???
 
     if not any(ele in sys.argv[2] for ele in sorting):                                  # did you provide a sort
         print("your process scheduling options are FCFS, ShortestFirst, or Priority")
@@ -71,18 +79,22 @@ def main():
     else:                                                                               # then parse the input
         with open(sys.argv[1], 'r') as f:
             for line in f:
-                pid, arrival_time, burst_time, priority = line.split()
-                pid_list += [pid]
-                arrival_time_list += [arrival_time]
-                burst_time_list += [burst_time]
-                priority_list += [priority]
+                pid, arrival_time, burst_time, priority = line.split(', ')
+                pid_list += [int(pid)]
+                arrival_time_list += [int(arrival_time)]
+                burst_time_list += [int(burst_time)]
+                priority_list += [int(priority)]
+
+    for elem in range(0, len(pid_list)):                                                # find completion times
+        completion = arrival_time_list[elem] + burst_time_list[elem]
+        completion_list += [completion]
 
     if sys.argv[2] == "FCFS":                                                           # call sorting and print
-        first_come_first_served_sort()
+        fcfs_ret = first_come_first_served_sort()
     elif sys.argv[2] == "ShortestFirst":
-        shortest_job_first_sort()
+        sjf_ret = shortest_job_first_sort()
     elif sys.argv[2] == "Priority":
-        priority_sort()
+        prio_ret = priority_sort()
     # For each algorithm, the output to the terminal should be the processes in the order that they should execute,
     # the average process waiting time, and the average process turn around time, each on their own line. All input
     # and output should be gathered and executed IN MAIN
