be9336293e95da2919574c62f79ede231631d843
PlutoThePlanet <pmortensen0916@gmail.com>
2022-03-20 00:28:06 -0700

    brain hurty, what I have so far for SJF

--- batchSchedulingComparison.py
+++ batchSchedulingComparison.py
@@ -2,11 +2,15 @@
 # Paige Mortensen
 # CS 446 PA2 - compare, contrast, and simulate process management algorithms
 
-# As part of your file header, compare and contrast the two algorithms.
+# As part of your file header, compare and contrast the three algorithms.
 # Be sure to explain where each would be appropriate to use,
 # and any possible tradeoffs in implementation or process execution
 
+# shortest job has a greater likelihood of encountering more interrupts
+# there might be a more efficient ordering for FCFS
+
 import sys
+from collections import deque
 
 
 def avg_turnaround(process_completion_times, process_arrival_times):
@@ -42,13 +46,41 @@ def first_come_first_served_sort(data):
 
 
 def shortest_job_first_sort(data):
-    #
+    data.sort(key=lambda x: x[1])               # sort by arrival time
+    queue = []
+    pid_list = []
+    index = 1
+    length = len(data)
+
+    for elem in range(0, len(data)):            # load all processes
+        queue.append(data[elem])
+
+    pid_list += [data[0][0]]                    # list first PID
+
+    while not queue == []:                          # while there are still processes waiting to be run
+        if queue[index][2] < queue[0][2]:           # if the process coming in has a shorter time than what's at front
+            queue.insert(index, data[index+1])      # place this new process in front
+            # update the burst of the old process
+            # add new process to list
+        else:
+            index += 1
+
+    # if the burst of the newest process in the queue is less than the remaining time to execute the current process,
+    # the current process should be added back to the queue and the new process should be executed
     return data
 
 
 def priority_sort(data):
-    #
-    return data
+    data.sort(key=lambda x: x[0])                       # sort by PID
+    data.sort(key=lambda x: x[3])                       # sort by priority
+    data.sort(key=lambda x: x[1])                       # sort by arrival time
+    # completion_list = []
+    # current_time = 0
+    # for elem in range(0, len(data)):  # find completion times
+    #     completion = current_time + data[elem][2]  # completion = currentTime + burstTime
+    #     completion_list += [completion]  # add to list
+    #     current_time += data[elem][2]  # new current time
+    # return [data, completion_list]
 
 
 def main():
@@ -84,14 +116,14 @@ def main():
                 data_2d.insert(index, data_list)
                 index += 1
 
-    if sys.argv[2] == "FCFS":                                                               # call sort fcts, and print
+    if sys.argv[2] == "FCFS":                                                               # FCFS
         fcfs_ret = first_come_first_served_sort(data_2d)
         data_2d = fcfs_ret[0]
         completion_list = fcfs_ret[1]
         for elem in range(0, len(data_2d)):
             print(data_2d[elem][0])
         for elem in range(0, 4):
-            pid_list += [int(data_2d[elem][0])]                                             # update arrays
+            pid_list += [int(data_2d[elem][0])]                                             # update arrays - sorted
             arrival_time_list += [int(data_2d[elem][1])]
             burst_time_list += [int(data_2d[elem][2])]
             priority_list += [int(data_2d[elem][3])]
